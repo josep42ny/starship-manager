845e58f11066f373164063624037f909a80fe433
josep42ny <82282886+josep42ny@users.noreply.github.com>
2026-03-24 11:20:16 +0100

    initial commit

---
+++ src/test/java/com/w2m/starshipmanager/StarshipManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.w2m.starshipmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarshipManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
