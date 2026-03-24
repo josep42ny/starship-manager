451bd01482dd0d2ab16d1edd806efade57f9c1b1
josep42ny <82282886+josep42ny@users.noreply.github.com>
2026-03-24 11:36:57 +0100

    feat: add starship entity

---
+++ src/main/java/com/w2m/starshipmanager/model/Starship.java
@@ -0,0 +1,36 @@
+package com.w2m.starshipmanager.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "starship")
+public class Starship {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "length")
+    private Double length;
+
+    @Column(name = "beam")
+    private Double beam;
+
+    @Column(name = "created_at")
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+}
