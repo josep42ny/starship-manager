361719369f6516ce1cc1cf417fada32b70ff6e53
josep42ny <82282886+josep42ny@users.noreply.github.com>
2026-03-26 10:42:15 +0100

    feat: add jwt auth filter from previous project

---
+++ src/main/java/com/w2m/starshipmanager/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,59 @@
+package com.w2m.starshipmanager.filter;
+
+import com.w2m.starshipmanager.data.model.JwtPrincipal;
+import com.w2m.starshipmanager.util.JwtUtils;
+import io.jsonwebtoken.JwtException;
+import jakarta.annotation.Nonnull;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(
+            @Nonnull final HttpServletRequest request,
+            @Nonnull final HttpServletResponse response,
+            @Nonnull final FilterChain filterChain
+    ) throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization");
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        try {
+            final String jwt = authHeader.substring(7);
+            final JwtPrincipal principal = JwtUtils.parseAndValidate(jwt);
+
+            final var authorities = principal.roles().stream()
+                    .map(SimpleGrantedAuthority::new)
+                    .toList();
+
+            final var authentication = new UsernamePasswordAuthenticationToken(
+                    principal.username(),
+                    null,
+                    authorities
+            );
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            filterChain.doFilter(request, response);
+        } catch (final JwtException | AuthenticationException e) {
+            throw new BadCredentialsException("Invalid JWT: " + e.getMessage(), e);
+        }
+    }
+}
